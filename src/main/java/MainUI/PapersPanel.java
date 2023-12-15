733c330e1bc9b8f7321205aba3ef84cf98ccf0fe
adelinavlasie <116746233+adelinavlasie@users.noreply.github.com>
2023-12-15 11:49:29 +0000

    Add panels to UI

--- src/main/java/MainUI/PapersPanel.java
+++ src/main/java/MainUI/PapersPanel.java
@@ -3,21 +3,51 @@ package MainUI;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PapersPanel extends JPanel {
 
-    //main area to show papers
+        private Papers_list papersList;
+        private JTextArea displayArea;
 
-    public PapersPanel() {
-        setSize(1100, 930);
-        setLocation(340, 20);
-        setVisible(true);
-        setBorder(BorderFactory.createLineBorder(Color.black));
+        public PapersPanel() {
+            papersList = new Papers_list();
+            setSize(1100, 930);
+            setLocation(340, 20);
+            setVisible(true);
+            setBorder(BorderFactory.createLineBorder(Color.black));
+
+            displayArea = new JTextArea(30, 50);
+            displayArea.setEditable(false);
+
+            JScrollPane scrollPane = new JScrollPane(displayArea);
+            add(scrollPane);
+
+            displayPapers();
+        }
+
+        public void displayPapers() {
+            ArrayList<String> references = papersList.getReferenceList();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (String reference : references) {
+                stringBuilder.append(reference).append("\n");
+            }
+
+            displayArea.setText(stringBuilder.toString());
 
 
+        }
 
 
     }
 
 
-}
+
+
+
+
+
+
+
+
