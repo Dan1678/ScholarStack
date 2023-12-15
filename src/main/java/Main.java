71a2de61d4bf52ed4d2d695fa357307e9369671e
Dan <dc1321@ic.ac.uk>
2023-12-15 10:15:00 +0000

    Changes by johnn

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String arg[])
+    {
+        MainUI mainUI = new MainUI();
+        try
+        {
+            //create instance of the CreateLoginForm
+            CreateLoginForm form = new CreateLoginForm();
+            form.setSize(300,100);  //set size of the frame
+            form.setVisible(true);  //make form visible to the user
+        }
+        catch(Exception e)
+        {
+            //handle exception
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/MainUI.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainUI extends JFrame {
+
+    static GraphicsConfiguration gc;	// Contains this computer’s graphics configuration
+
+
+    public MainUI() {
+        setSize(500,300);
+        setLayout(null); // using no layout managers
+        setVisible(true); // making the frame visible
+
+
+
+        // This next line closes the program when the frame is closed
+        addWindowListener(new WindowAdapter() {	// Closes the program if close window clicked
+            public void windowClosing(WindowEvent e) {
+                dispose();
+            }
+        });
+    }
+
+    
+
+}
