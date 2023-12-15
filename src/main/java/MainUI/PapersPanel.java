d57a9c8715267b5ed43fb97a7e41b6fa08b5b8da
adelinavlasie <116746233+adelinavlasie@users.noreply.github.com>
2023-12-15 16:51:38 +0000

    add ref Harvard and scroll bars
    add refrence fixed, scroll bars, Harvard ref type

--- src/main/java/MainUI/PapersPanel.java
+++ src/main/java/MainUI/PapersPanel.java
@@ -6,6 +6,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PapersPanel extends JPanel {
 
@@ -14,32 +16,47 @@ public class PapersPanel extends JPanel {
 
         public PapersPanel() {
 
+            setLayout(new BorderLayout());
+            papersList = new Papers_list();
             //Set up panel
             setVisible(true);
             setBorder(BorderFactory.createLineBorder(Color.black));
 
 
-            papersList = new Papers_list();
 
-            displayArea = new JTextArea(30, 50);
+
+            displayArea = new JTextArea(250, 505);
             displayArea.setEditable(false);
 
             JScrollPane scrollPane = new JScrollPane(displayArea);
-            add(scrollPane);
+            scrollPane.setPreferredSize(new Dimension(500, 300));//make it large otherwise it won t show
+            add(scrollPane,BorderLayout.CENTER);
             JButton addReferenceButton = new JButton("Add Reference");
             addReferenceButton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     String reference = JOptionPane.showInputDialog("Enter the Harvard reference:");
-                    if (reference != null && !reference.isEmpty()) {
+                    if (reference != null && !reference.isEmpty()&& isValidHarvardReference(reference)) {
                         papersList.addPaper(reference);
                         displayPapers();
+                    }else {
+                        JOptionPane.showMessageDialog(null, "Please enter a valid Harvard reference. Make sure to use , in between and . at the end");
                     }
                 }
             });
-            add(addReferenceButton);
+            JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER)); // FlowLayout for center alignment
+            buttonPanel.add(addReferenceButton);
+            add(buttonPanel, BorderLayout.SOUTH);
 
             displayPapers();
+           // JPanel buttonPanel = new JPanel();
+            //buttonPanel.add(addReferenceButton);
+
+           // setLayout(new BorderLayout());
+           // add(addReferenceButton,BorderLayout.SOUTH);
+            //add(scrollPane,BorderLayout.CENTER);
+
+
         }
 
         public void displayPapers() {
@@ -54,9 +71,15 @@ public class PapersPanel extends JPanel {
             displayArea.setText(stringBuilder.toString());
 
         }
-
-
+        private boolean isValidHarvardReference(String reference) {
+        Pattern harvardPattern = Pattern.compile("^([A-Za-z]+), ([A-Za-z]+), \\d{4}\\.$");
+        Matcher matcher = harvardPattern.matcher(reference);
+        return matcher.matches();
     }
+
+
+
+}
 
 
 
--- src/main/java/MainUI/Papers_list.java
+++ src/main/java/MainUI/Papers_list.java
@@ -5,26 +5,23 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class Papers_list {
-    ArrayList<String> Papers = new ArrayList<String>();
-    Scanner scanner = new Scanner(System.in);
-    public String paper;
+/*class Papers_list {
+    private ArrayList<String> Papers = new ArrayList<>();
+
+
 
     public void addPaper(String reference) {
-        Papers.add(scanner.next());
-        boolean validInput = false;
-        String userInput;
+
+
         Pattern harvardPattern = Pattern.compile("^([A-Z][a-zA-Z]+), ([A-Z][a-zA-Z]+), ([0-9]{4})\\.$");
+        Matcher matcher = harvardPattern.matcher(reference);
 
-        while (!validInput) {
-            System.out.println("Enter the Harvard reference (Author Last Name, Author First Name, Year): ");
-            userInput = scanner.nextLine();
 
-            Matcher matcher = harvardPattern.matcher(userInput);
+
 
             if (matcher.matches()) {
-                Papers.add(userInput);
-                validInput = true;
+                Papers.add(reference);
+                ;
             } else {
                 System.out.println("Please enter the reference in Harvard format (Last Name, First Name, Year).");
             }
@@ -34,12 +31,36 @@ class Papers_list {
 
 
     public ArrayList<String> getReferenceList() {
-        return Papers ;
-    }
-}
 
+
+        return Papers;
+    }
+
+}
 
 //for (int i = 0; i < Papers.size(); i++) {
        // System.out.println(cars.get(i));
+*/
+public class Papers_list {
+    private ArrayList<String> Papers = new ArrayList<>();
+
+    public void addPaper(String reference) {
+        //Pattern harvardPattern = Pattern.compile("^([A-Z][a-zA-Z]+), ([A-Z][a-zA-Z]+), ([0-9]{4})\\.$");
+      /*  Pattern harvardPattern = Pattern.compile("^([A-Za-z]+) , ([A-Za-z]+), \\d{4}\\.$");
+        Matcher matcher = harvardPattern.matcher(reference);
+
+        if (matcher.matches()) {
+            Papers.add(reference);
+        } else {
+            System.out.println("Please enter the reference in Harvard format (Last Name, First Name, Year).");
+        }*/
+
+            Papers.add(reference);
+    }
+
+    public ArrayList<String> getReferenceList() {
+        return Papers;
+    }
+}
 
 
