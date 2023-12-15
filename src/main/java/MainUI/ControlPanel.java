0645d281b21103987920c6f129d340dbb03f4f8b
BonniciJ <jab21@ic.ac.uk>
2023-12-15 13:34:10 +0000

    Add panels to UI

--- src/main/java/MainUI/ControlPanel.java
+++ src/main/java/MainUI/ControlPanel.java
@@ -12,11 +12,19 @@ public class ControlPanel extends JPanel {
         //Browse tags
         //Add tags
 
+    private JButton addPaperBtn;
+
     public ControlPanel() {
         setSize(300, 930);
         setPreferredSize(new Dimension(500, 0));
         setVisible(true);
         setBorder(BorderFactory.createLineBorder(Color.black));
+
+
+        addPaperBtn = new JButton("Add Paper");
+        addPaperBtn.setFont(new Font("Arial", Font.BOLD, 24));
+        addPaperBtn.setPreferredSize(new Dimension(200, 75));
+        add(addPaperBtn, BorderLayout.NORTH);
     }
 
 }
