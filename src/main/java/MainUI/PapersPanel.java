2b3f5a8154339e961edf5c403fc6ba58dd8002e5
adelinavlasie <116746233+adelinavlasie@users.noreply.github.com>
2023-12-16 16:31:33 +0000

    added get bibliography options and changed display refrences so they are in numerical order

--- src/main/java/MainUI/PapersPanel.java
+++ src/main/java/MainUI/PapersPanel.java
@@ -44,8 +44,34 @@ public class PapersPanel extends JPanel {
                     }
                 }
             });
+
+            JButton generateBibliographyButton = new JButton("Generate Bibliography");
+            generateBibliographyButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    generateBibliography();
+                }
+            });
+            JButton addCommentButton = new JButton("Add Comment");
+            addCommentButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String selectedReference = getSelectedReference();
+                    if (selectedReference != null) {
+                        String comment = JOptionPane.showInputDialog("Enter a comment for this reference:");
+                        if (comment != null && !comment.isEmpty()) {
+                            papersList.addComment(selectedReference, comment);
+                            displayPapers();
+                        }
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Please select a reference to add a comment.");
+                    }
+                }
+            });
             JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER)); // FlowLayout for center alignment
             buttonPanel.add(addReferenceButton);
+            buttonPanel.add(generateBibliographyButton);
+            buttonPanel.add(addCommentButton);
             add(buttonPanel, BorderLayout.SOUTH);
 
             displayPapers();
@@ -64,8 +90,11 @@ public class PapersPanel extends JPanel {
             ArrayList<String> references = papersList.getReferenceList();
             StringBuilder stringBuilder = new StringBuilder();
 
-            for (String reference : references) {
+            /*for (String reference : references) {
                 stringBuilder.append(reference).append("\n");
+            }*/
+            for (int i = 0; i < references.size(); i++) {
+                stringBuilder.append(i + 1).append(". ").append(references.get(i)).append("\n");
             }
 
             displayArea.setText(stringBuilder.toString());
@@ -76,10 +105,41 @@ public class PapersPanel extends JPanel {
         Matcher matcher = harvardPattern.matcher(reference);
         return matcher.matches();
     }
+    private void generateBibliography() {
+        ArrayList<String> references = papersList.getReferenceList();
+        StringBuilder bibliography = new StringBuilder("Bibliography:\n");
 
+        /*for (String reference : references) {
+            bibliography.append("- ").append(reference).append("\n");
+        }*/
+        for (int i = 0; i < references.size(); i++) {
+            bibliography.append(i + 1).append(". ").append(references.get(i)).append("\n");
+        }
 
+        // Display the bibliography in a dialog
+        JTextArea bibliographyTextArea = new JTextArea(bibliography.toString(), 20, 50);
+        bibliographyTextArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(bibliographyTextArea);
 
+        JOptionPane.showMessageDialog(null, scrollPane, "Generated Bibliography", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    private String getSelectedReference() {
+        // Fetch the selected reference from your UI element (e.g., displayArea)
+        // For example, if displayArea is a JTextArea:
+        String selectedText = displayArea.getSelectedText();
+        if (selectedText != null && !selectedText.isEmpty()) {
+            // Extract the reference from the selected text (customize based on your format)
+            // For now, assuming each reference is on a new line
+            return selectedText.trim(); // Trim any extra spaces
+        }
+        return null;
+    }
 }
+
+
+
+
 
 
 
--- src/main/java/MainUI/Papers_list.java
+++ src/main/java/MainUI/Papers_list.java
@@ -1,65 +1,38 @@
 package MainUI;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/*class Papers_list {
-    private ArrayList<String> Papers = new ArrayList<>();
-
-
-
-    public void addPaper(String reference) {
-
-
-        Pattern harvardPattern = Pattern.compile("^([A-Z][a-zA-Z]+), ([A-Z][a-zA-Z]+), ([0-9]{4})\\.$");
-        Matcher matcher = harvardPattern.matcher(reference);
 
 
 
 
-            if (matcher.matches()) {
-                Papers.add(reference);
-                ;
-            } else {
-                System.out.println("Please enter the reference in Harvard format (Last Name, First Name, Year).");
-            }
-        }
-    }
 
 
 
-    public ArrayList<String> getReferenceList() {
 
-
-        return Papers;
-    }
-
-}
-
-//for (int i = 0; i < Papers.size(); i++) {
-       // System.out.println(cars.get(i));
-*/
 public class Papers_list {
     private ArrayList<String> Papers = new ArrayList<>();
+    private Map<String, String> comments = new HashMap<>();
 
     public void addPaper(String reference) {
-        //Pattern harvardPattern = Pattern.compile("^([A-Z][a-zA-Z]+), ([A-Z][a-zA-Z]+), ([0-9]{4})\\.$");
-      /*  Pattern harvardPattern = Pattern.compile("^([A-Za-z]+) , ([A-Za-z]+), \\d{4}\\.$");
-        Matcher matcher = harvardPattern.matcher(reference);
 
-        if (matcher.matches()) {
-            Papers.add(reference);
-        } else {
-            System.out.println("Please enter the reference in Harvard format (Last Name, First Name, Year).");
-        }*/
 
             Papers.add(reference);
+    }
+    public void addComment(String reference, String comment) {
+        comments.put(reference, comment);
     }
 
     public ArrayList<String> getReferenceList() {
         return Papers;
+    }
+    public Map<String, String> getCommentsMap() {
+        return comments;
     }
 }
 
