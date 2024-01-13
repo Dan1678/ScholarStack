cdc23286800bdd13c7f385ce7fee83c097831b07
Dan <dc1321@ic.ac.uk>
2024-01-13 17:07:48 +0000

    Adding logging to the error handling in the LoginManager class

--- src/main/java/Managers/LoginManager.java
+++ src/main/java/Managers/LoginManager.java
@@ -2,9 +2,11 @@ package Managers;
 
 import Managers.DatabaseConnector;
 
+import java.io.IOException;
 import java.sql.*;
 
 public class LoginManager {
+    static Log log;
     public static boolean isUserTaken(String userName) {
         try (Connection conn = DatabaseConnector.connectToDatabase()) {
             if (conn != null) {
@@ -19,6 +21,14 @@ public class LoginManager {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                // Specify the log file name when creating the Log instance
+                log = new Log("log.txt");
+                log.logger.severe("SQL Exception:" + e.getMessage());
+            } catch (IOException p) {
+                log.logger.warning("Log unsuccessful:" + p.getMessage());
+            }
+
         }
         return false;     //if username does not exist
     }
@@ -39,6 +49,13 @@ public class LoginManager {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            try {
+                // Specify the log file name when creating the Log instance
+                log = new Log("log.txt");
+                log.logger.severe("SQL Exception:" + e.getMessage());
+            } catch (IOException p) {
+                log.logger.warning("Log unsuccessful:" + p.getMessage());
+            }
         }
         return false;      //if user not found or passwords dont match
     }
